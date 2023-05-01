707879cdb6e237c961386878089057d02c6f611c
HasibulHasan <hasibulhasan372@gmail.com>
2023-05-02 05:09:45 +0600

    recipe.json and recipe api created

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const chef = require('./data/chef.json')
+const chef = require('./data/chef.json');
+const recipe = require('./data/recipe.json')
 
 app.get('/', (req, res) =>{
     res.send('Hello from Iranian Food')
 })
 app.get('/chef',(req, res) =>{
     res.send(chef)
+});
+app.get('/recipe', (req, res) =>{
+    res.send(recipe)
+});
+app.get('/recipe/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const selectedRecipe = recipe.find(menu => menu.id === id);
+    res.send(selectedRecipe)
+});
+app.get('/chef/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id)
+    if(id === "ifh_0"){
+        res.send(recipe)
+    }
+    else{
+        const chefRecipe = recipe.filter(menu => menu.chef_id === id)
+        res.send(chefRecipe)
+    }
 })
 app.listen(port, () =>{
     console.log(`Iranian Food Hub API is running on port: ${port}`)
