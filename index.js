aa7ebd8cf982542bacd077da0a9ad74e1bd923e6
HasibulHasan <hasibulhasan372@gmail.com>
2023-05-02 04:42:38 +0600

    create chef.json api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const chef = require('./data/chef.json')
 
 app.get('/', (req, res) =>{
     res.send('Hello from Iranian Food')
 })
-
+app.get('/chef',(req, res) =>{
+    res.send(chef)
+})
 app.listen(port, () =>{
     console.log(`Iranian Food Hub API is running on port: ${port}`)
 })
